81d197b1f9f4a42d483c1cd9e6803580c8c18ec6
Nils Bore <nilsbore@gmail.com>
2019-11-01 09:03:07 +0100

    CMake part of submodule changes

---
+++ cmake/FindBullet.cmake
@@ -0,0 +1,9 @@
+
+list (APPEND BULLET_LIBRARIES LinearMath)
+list (APPEND BULLET_LIBRARIES Bullet3Common)
+list (APPEND BULLET_LIBRARIES BulletInverseDynamics)
+list (APPEND BULLET_LIBRARIES BulletCollision)
+list (APPEND BULLET_LIBRARIES BulletDynamics)
+list (APPEND BULLET_LIBRARIES BulletSoftBody)
+
+set(BULLET_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/external/bullet3/src)
